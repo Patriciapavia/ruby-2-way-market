5399306a490a7a2afd29db2d618607eff226d5ea
patricia <patriciapavia236@gmail.com>
2020-05-09 13:01:30 +1000

    adding route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'items#index'
+  resource :items
+
+
+  # get "/items", to: "items#index", as: "root"
+  # post "/items", to: "items#create"
+  # get "/items/new", to: "items#new", as: "new_items"
+  # get "/items/:id", to: "items#show", as: "item"
+  # put "/items/:id", to: "items#update"
+  # patch "/items/:id", to: "items#update"
+  # delete "/items/:id", to: "items#destroy"
+  # get "/items/:id/edit", to: "items#edit", as: "edit_items"
+  
+  
 end
