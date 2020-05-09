3e1a2dfec7c050c53d45c7a5ee335b23c7101b38
patricia <patriciapavia236@gmail.com>
2020-05-09 15:15:17 +1000

    addshowview

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-
+ before_action :find_item, only: [:show, :edit, :update, :destroy]
 
   def index
   	@items = Item.all
@@ -12,18 +12,31 @@ class ItemsController < ApplicationController
   def create
   	@item = Item.new(item_params)
   	if @item.save
-  		redirect_to root_path
+  		redirect_to items_path
   	else
   		render "new"
   	end
 
   end
 
+  def show
+    
+    
+
+  end
+
 
    private
+
+   def find_item
+    @item = Item.find(params[:id])
+   end
+
+
    def item_params
    	params.require(:item).permit(:title, :description, :size, :color, :price)
    	
    end
 
+   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
 
-  root 'items#index'
-  resource :items
+  # root 'items#index'
+  # resource :items
 
-
-  # get "/items", to: "items#index", as: "root"
-  # post "/items", to: "items#create"
-  # get "/items/new", to: "items#new", as: "new_items"
-  # get "/items/:id", to: "items#show", as: "item"
-  # put "/items/:id", to: "items#update"
-  # patch "/items/:id", to: "items#update"
-  # delete "/items/:id", to: "items#destroy"
-  # get "/items/:id/edit", to: "items#edit", as: "edit_items"
+  get "/items", to: "items#index", as: "items"
+  post "/items", to: "items#create"
+  get "/items/new", to: "items#new", as: "new_items"
+  get "/items/:id", to: "items#show", as: "item"
+  put "/items/:id", to: "items#update"
+  patch "/items/:id", to: "items#update"
+  delete "/items/:id", to: "items#destroy"
+  get "/items/:id/edit", to: "items#edit", as: "edit_items"
   
   
 end
